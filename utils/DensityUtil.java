c8c1200659506ff54da58bb7a142875dea92fb21
ssyijiu <ssyijiu@126.com>
2016-07-28 15:44:38 +0800

    update DensityUtil

--- utils/DensityUtil.java
+++ utils/DensityUtil.java
@@ -1,34 +1,25 @@
 package com.ssyijiu.alertdialog.ui;
+
+
 import android.content.Context;
+import android.content.res.Resources;
 
 public class DensityUtil {
 
-    private DensityUtil(){
-		/* cannot be instantiated */
-        throw new UnsupportedOperationException("cannot be instantiated");
-    }
 
-    /**
-     * 根据手机的分辨率从 dp 的单位 转成为 px(像素) 
-     */  
-    public static float dp2px(Context context, float dpValue) {
+    public static int dp2px(Context context, float dpValue) {  
         final float scale = context.getResources().getDisplayMetrics().density;
-        return dpValue * scale + 0.5f;
+        return (int) (dpValue * scale + 0.5f); 
     }  
-  
-    /** 
-     * 根据手机的分辨率从 px(像素) 的单位 转成为 dp 
-     */  
-    public static float px2dp(Context context, float pxValue) {
+
+
+    public static int px2dp(Context context, float pxValue) {  
         final float scale = context.getResources().getDisplayMetrics().density;  
-        return pxValue / scale + 0.5f;
+        return (int) (pxValue / scale + 0.5f);  
     }
 
-    /**
-     * 根据手机的分辨率从 sp 的单位 转成为 px(像素)
-     */
-    public static float sp2px(Context context, float spValue){
-        final float scale = context.getResources().getDisplayMetrics().scaledDensity;
-        return spValue * scale + 0.5f;
+    public static int sp2px(Resources resources, float spValue){
+        final float scale = resources.getDisplayMetrics().scaledDensity;
+        return (int) (spValue * scale + 0.5f);
     }
 }
